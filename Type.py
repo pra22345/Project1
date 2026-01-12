e2120ee0625b2d44a180f1029a0d81f22cf294f0
pra22345 <pradnyapatil770@gmail.com>
2026-01-12 11:54:32 +0530

    Add files via upload

---
+++ Type.py
@@ -0,0 +1,47 @@
+# x=10
+# print("x is of type:",type(x))
+# y=10.6
+# print("y is of type:",type(y))
+# z=x+y
+# print(z)
+# print("z is off type:",type(z))
+
+
+# import math
+# print("the value of log 2 with base 3 is:",end="")
+# print(math.log(2,3))
+# print("the value of log2 of 16 is:",end="")
+# print(math.log2(16))
+# print("the;
+# value of log10 of 10000 is:",end="")
+# print(math.log10(10000))
+
+
+# import math
+# a=math.pi/6 
+# print("the value of sine of pi/6 is:",end=" ")
+# print(math.sin(a))
+# print("the value of sine of pi/6 is:",end=" ")
+# print(math.cos(a))
+# print("the value of sine of pi/6 is:",end=" ")
+# print(math.tan(a))
+
+
+
+import pandas as pd
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv("data.csv")
+
+# Print the entire DataFrame
+print(df.to_string())
+
+# import pandas as pd
+# df = pd.read_json('data.json')
+# print(df.to_string())
+
+
+
+
+
+
