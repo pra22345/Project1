1ee326206be8d85ced13febcb1e02189050ca3a8
pra22345 <pradnyapatil770@gmail.com>
2026-01-12 11:47:14 +0530

    Add files via upload

---
+++ Example.py
@@ -0,0 +1,66 @@
+#t=(1,2,3,4,5)
+#print(t[1])
+#print(t[4])
+#t=(1,2,3,4,2,3)
+#print(t)
+#t[1]=100
+#print(t)
+
+#t=(10,5,20)
+#print("value in t[-1]=",t[-1])
+#print("value in t[-2]=",t[-2])
+#print("value in t[-3]=",t[-3])
+
+
+
+#t=('python',)*2
+#print(t)
+
+#t=(0,1)
+#del t
+#print(t)
+
+#a=[0,1,2]
+#t=tuple(a)
+#print(t)
+
+#a=[10,20,30]
+#a.remove(30)
+#print(a)
+
+
+# s={'g','e','k','s'}
+# s.add('f')
+# print('set after updating:',s)
+# s.discard('g')
+# print('\n set after updating:',s)
+# s.remove('e')
+# print('\n set after updating:',s)
+# print('\npopped elements',s.pop())
+# print('\nset after updating',s)
+# s.clear()
+# print('\nset after updating',s)
+
+
+mylist=['college','university','country']
+#x=frozenset(mylist)
+x=mylist
+x[1]="college_1"
+
+
+
+
+
+
+
+
+
+
+
+                                
+
+
+
+
+
+
